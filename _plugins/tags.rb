d8e34832c9cb3e9fc240bf72f18c81715ff86b42
Brandon Mathis <brandon@imathis.com>
2018-08-16 09:39:08 -0500

    Fixed tags

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -41,6 +41,38 @@ module Jekyll
       end
     end
 
+    class Card < BaseBlock
+
+      def render(context)
+        @options["class"].push "card"
+        card { buttonize(markdown(super)) }
+      end
+    end
+
+    class Hours < BaseTag
+      def render(context)
+        super
+        hours = @data['hours']
+        now = Time.now
+
+        # DST Hours
+        h = hours[@args.first] || if now.isdst
+          hours["dst"]
+        # Off Season January & February
+        elsif hours["off-season"] && [1,2].include?(now.month)
+          hours["off-season"]
+        # Normal Hours
+        else
+          hours["default"]
+        end
+
+        h = Array(h).join("<br>")
+        
+        content_tag(:span, h, class: 'hours')
+
+      end
+    end
+
     class EventCard < BaseTag
       def render(context)
         super
@@ -100,11 +132,12 @@ module Jekyll
   end
 end
 
-Liquid::Template.register_tag('figure', Jekyll::Tags::Figure)
+#Liquid::Template.register_tag('figure', Jekyll::Tags::Figure)
 Liquid::Template.register_tag('header', Jekyll::Tags::Header)
 Liquid::Template.register_tag('link_to', Jekyll::Tags::LinkTo)
 Liquid::Template.register_tag('link', Jekyll::Tags::LinkBlock)
 Liquid::Template.register_tag('event_card', Jekyll::Tags::EventCard)
 Liquid::Template.register_tag('contact', Jekyll::Tags::Contact)
-Liquid::Template.register_tag('markdown', Jekyll::Tags::Markdown)
+#Liquid::Template.register_tag('markdown', Jekyll::Tags::Markdown)
 Liquid::Template.register_tag('box', Jekyll::Tags::Box)
+Liquid::Template.register_tag('hours', Jekyll::Tags::Hours)
